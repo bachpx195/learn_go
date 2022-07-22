d64146c86383c24ffd5cbb50e8b573215e3127d5
Bachpx <bachpx@ponos-tech.com>
2022-07-22 09:10:12 +0700

    update

--- main.go
+++ main.go
@@ -1,32 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
 
-//
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
 
-// func main() {
-// 	fmt.Println("Hello World")
-// }
+type Restaurant struct {
+	Id     int             `json:"id" gorm:"column:id;"`
+	Name   string          `json:"name" gorm:"column:name;"`
+	Addr   string          `json:"addr" gorm:"column:addr;"`
+	Logo   json.RawMessage `json:"logo" gorm:"column:logo;"`
+	Status int             `json:"status" gorm:"column:status;"`
+}
+
+func (Restaurant) TableName() string { return "restaurants" }
 
 func main() {
-	var stationName string
-	var nextTrainTime int8
-	var isUptownTrain bool
+	fmt.Println("bach")
+	dsn := "root:quachtinh95@tcp(127.0.0.1:3306)/learn_go?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	stationName = "Union Square"
-	nextTrainTime = 12
-	isUptownTrain = false
+	log.Println(err)
 
-	fmt.Println("Current station:", stationName)
-	fmt.Println("Next train:", nextTrainTime, "minutes")
-	fmt.Println("Is uptown:", isUptownTrain)
+	r := gin.Default()
 
-	stationName = "Grand Central"
-	nextTrainTime = 3
-	isUptownTrain = true
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
 
-	fmt.Println("")
-	fmt.Println("Current station:", stationName)
-	fmt.Println("Next train:", nextTrainTime, "minutes")
-	fmt.Println("Is uptown:", isUptownTrain)
+	r.Run()
+
+	newRestaurant := Restaurant{Name: "Tani", Addr: "Da nang", Status: 1}
+
+	if err := db.Create(&newRestaurant).Error; err != nil {
+		log.Println(err)
+	}
+
+	var myRestaurant Restaurant
+
+	if err := db.Where("id = ?", newRestaurant.Id).First(&myRestaurant).Error; err != nil {
+		log.Println(err)
+	}
+
+	log.Println(myRestaurant)
+
+	myRestaurant.Name = "bach"
+	if err := db.Where("id = ?", 2).Updates(&myRestaurant).Error; err != nil {
+		log.Println(err)
+	}
+
+	log.Println(myRestaurant)
 }
